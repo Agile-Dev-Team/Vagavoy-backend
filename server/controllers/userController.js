f6c30e6755feeda94ba18ac9286d9561fe44462a
accompany1205 <accompaniedwithyou@gmail.com>
2022-10-24 21:44:06 +0100

    change trip id to triplogid in res

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -238,6 +238,7 @@ function removeBannerImage(req, res, next) {
 function findTravelByUserId(req, res, next) {
   Travel.find({userId: req.params.userId})
   .then((travels) => {
+    travels.map(travel => travel.tripLogId = travel._id)
     res.json(travels)
   })
   .catch(next)
